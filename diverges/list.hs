a1f4aebda2c16d306a4d191702b15bed867bca0d
Charles-Pierre Astolfi <cpa@crans.org>
2011-05-20 12:25:07 +0200

    Fixed: list.hs

--- diverges/list.hs
+++ diverges/list.hs
@@ -15,8 +15,12 @@ notZero x = case x of
   | Nil -> False
   | Cons a b -> True;;
     
+not x = case x of
+  | True -> False
+  | False -> True;;
+
 f x = case x of 
     | Nil -> Nil
     | Cons a b -> Nil;;
     
-f ::: a:{x : True} -> {y : notZero (length y)};;
+f ::: a:{x : True} -> {y : not (notZero (length y))};;
