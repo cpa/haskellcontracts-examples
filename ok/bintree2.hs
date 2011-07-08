5dc12baab8335540b2b50264bff5176808a44e83
Charles-Pierre Astolfi <cpa@crans.org>
2011-07-08 19:41:07 +0200

    New binary tree test

---
+++ ok/bintree2.hs
@@ -0,0 +1,25 @@
+data Tree = Leaf 0
+          | Node 2;;
+                    
+data Int = Zero 0
+         | Succ 1;;
+    
+isLeaf t = case t of
+  | Leaf -> True
+  | Node a b -> False;;
+
+eq t1 t2 = case t1 of
+  | Leaf -> isLeaf t2
+  | Node a b -> aux t2 a b;;
+    
+aux t a b = case t of
+  | Leaf -> False
+  | Node c d -> and (eq a c) (eq b d);;
+
+and x y = case x of
+  | True -> y
+  | False -> False;;
+    
+-- eq ::: a:{x:cf(x)} -> b:{y:cf(y)} -> {z:cf(z)};;
+isLeaf ::: a:{y:cf(y)} -> {z:cf(z)};;
+and ::: a:{x:cf(x)} -> b:{y:cf(y)} -> {z:cf(z)};;
