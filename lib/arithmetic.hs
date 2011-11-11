a6e4c773e7ad0e9573217f116b93f887b2b61270
Nathan Collins <nathan.collins@gmail.com>
2011-11-11 14:54:11 +0000

    Expanded lib/arithmetic.hs.

--- lib/arithmetic.hs
+++ lib/arithmetic.hs
@@ -1,6 +1,9 @@
 data Nat = Zero 0
          | Succ 1;;
 
+one = Succ Zero;;
+two = Succ one;;
+
 add x y = case x of
   | Zero -> y
   | Succ x_ -> Succ (add x_ y);;
@@ -13,3 +16,22 @@ notZero x = case x of
   | Zero -> False
   | Succ x_ -> True;;
 
+isZero x = case x of
+  | Zero -> True
+  | Succ x_ -> False;;
+
+eqNat x y = case x of
+  | Zero -> isZero y
+  | Succ x_ -> eqNatAux x_ y;;
+
+eqNatAux x y = case y of
+  | Zero -> False
+  | Succ y_ -> eqNat x y_;;
+
+gt x y = case x of
+  | Zero -> False
+  | Succ x_ -> gtAux x_ y;;
+
+gtAux x y = case y of
+  | Zero -> True
+  | Succ y_ -> gt x y_;;
