bad13fece9b71bd5a91e0a9ac36c536ef5ff0746
Nathan Collins <nathan.collins@gmail.com>
2011-11-17 13:10:48 +0000

    Added example illustrating that multiple contract translation bug.
    I had previously noticed that multiple contracts on the same function
    would produce the wrong results, but I just realized we have the same
    problem when we have multiple contracts on mutually recursive functions.
    In the translation we produce, we get a refutation if *any* of the
    contracts hold, instead of only when they *all* hold.

---
+++ no/mutual-recursion-bug.hs
@@ -0,0 +1,29 @@
+-- The contract for 'f' does not hold, but with the incorrect
+-- translation of multiple contracts, we conclude that it does!  The
+-- problem is that we must check 'f' and 't' concurrently, since
+-- they're mutually recursive, but the refutation we produce is wrong:
+--
+--   C |- phi_1 /\ phi_2 =trans=> C, ~ phi_1, ~ phi_2 |- _|_
+--
+-- but it should be
+--
+--   C |- phi_1 /\ phi_2 =trans=> C, ~ (phi_1 /\ phi_2) |- _|_
+import Lib.Prelude ;;
+
+t = case True of
+    ; True -> True
+    ; False -> f
+;;
+
+f = case True of
+    ; True -> False
+    ; False -> t
+;;
+
+{-# CONTRACT
+t ::: {r:r}
+#-};;
+{-# CONTRACT
+f ::: {r:r}
+#-};;
+                 
